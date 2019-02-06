7f3d3b158497bf8d3cd83f87490da10c2f5362df
ReanSn0w <re@nsnow.xyz>
2019-02-06 20:03:25 +0300

    Использоване localStorage для хранения информации

--- events.js
+++ events.js
@@ -6,12 +6,15 @@ function PlayerjsEvents(event,id,info){
         player.api("next");
         player.api("play");
     }
+    if(event=="pause"){
+        stateSaver();
+    }
  }
 
  function loadState() {
-    var cookie = getCookie(cleanId);
+    var cookie = localStorage.getItem(cleanId);
     if (cookie == undefined) {
-        stateSaver();
+        stateSaver().then();
         return
     }
 
@@ -20,35 +23,16 @@ function PlayerjsEvents(event,id,info){
 
     player.api(find, num);
     player.api(seek, second);
-    stateSaver();
+    stateSaver().then();
  }
 
 async function stateSaver() {
     for (i=true; i; i=true) {
-        //Сон перед сохранением позиции
-        sleep(15000);
         //Подготовка объекта для cookie
         var obj = {
             num: player.api("playlist_id"),
             second: player.api("second"),
         };
-        //Удаление старой куки
-        var date = new Date(0);
-        document.cookie = cleanId + "=; path=/; expires=" + date.toUTCString();
-        //Запись новой
-        document.cookie = cleanId + "=" + obj
+        localStorage.setItem(cleanId, obj);
     }
  }
-
- function getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
-
-//Ебаный косталь для sleep
-function sleep(ms) {
-    ms += new Date().getTime();
-    while (new Date() < ms){}
- } 
